019f4da6d222f1b3d17998eeb41804dc390633fd
vadim <vadim@XCSENON>
2023-12-12 16:23:03 +0200

    Добавьте файлы проекта.

---
+++ zmist/zmist.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+
+// Subject
+class Subject {
+public:
+    virtual void request() const = 0;
+};
+
+// RealSubject
+class RealSubject : public Subject {
+public:
+    void request() const override {
+        std::cout << "RealSubject request" << std::endl;
+    }
+};
+
+// Proxy
+class Proxy : public Subject {
+private:
+    RealSubject* const realSubject;  // Змінено на константний вказівник
+
+public:
+    Proxy() : realSubject(new RealSubject()) {}
+
+    void request() const override {
+        realSubject->request();
+    }
+
+    ~Proxy() {
+        delete realSubject;
+    }
+};
+
+int main() {
+    Proxy proxy;
+    proxy.request();
+
+    return 0;
+}
